b179594123720fd3e2bfc1554e575940175c3ae8
Gayatrimhase021 <gayatrimhase879@gmail.com>
2026-03-07 12:06:36 +0530

    added js code

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,13 @@
-<script>
-    console.log("Hello")
-</script>
+let inquiries = JSON.parse(localStorage.getItem("trekData")) || [];
+
+const form = document.getElementById("contactForm");
+const list = document.getElementById("inquiryList");
+
+function displayData(){
+
+list.innerHTML="";
+
+if(inquiries.length===0){
+list.innerHTML="<p>No inquiries yet.</p>";
+return;
+}
