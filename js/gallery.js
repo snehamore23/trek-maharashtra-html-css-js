b37bedcea8662756cb0b3a09853295c0e2787b84
snehamore23 <moresneha806@gmail.com>
2026-03-07 23:34:42 +0530

    js updated

--- js/gallery.js
+++ js/gallery.js
@@ -185,3 +185,12 @@ galleryPreviewContainer.innerHTML = previewHTML
 
 createGalleryPreview()
 showCurrentIndexImage()
+
+function toggleMenu(){
+
+let menu = document.getElementById("navMenu");
+let auth = document.querySelector(".auth-links-containar");
+
+menu.classList.toggle("active");
+auth.classList.toggle("active");
+}
