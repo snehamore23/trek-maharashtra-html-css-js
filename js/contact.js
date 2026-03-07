440263c4557c2dd216bf0a982ec802d127f0c375
Gayatrimhase021 <gayatrimhase879@gmail.com>
2026-03-07 22:31:00 +0530

    added navar with css and js

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,11 @@
+function toggleMenu(){
+
+let menu = document.getElementById("navMenu");
+let auth = document.querySelector(".auth-links-containar");
+
+menu.classList.toggle("active");
+auth.classList.toggle("active");
+}
 let inquiries = JSON.parse(localStorage.getItem("trekData")) || [];
 
 const form = document.getElementById("contactForm");
